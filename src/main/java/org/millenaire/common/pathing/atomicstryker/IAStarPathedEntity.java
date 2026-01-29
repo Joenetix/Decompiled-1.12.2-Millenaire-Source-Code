b24ec8f2373a24d3f52c94302cb5551170abd502
Joenetix <sullivanejoey@gmail.com>
2026-01-29 06:10:59 -0500

    Clean repository - only 1.12.2 Millenaire source code
    Removed 1.20.1 port code that was accidentally mixed in.
    Repository now contains only the decompiled 1.12.2 Millenaire source with fixes:
    - Fixed invisible villagers (MillWorldData instance mismatch)
    - Fixed resource loading errors
    - Updated models and textures

--- src/main/java/org/millenaire/common/pathing/atomicstryker/IAStarPathedEntity.java
+++
@@ -1,9 +0,0 @@
-package org.millenaire.common.pathing.atomicstryker;
-
-import java.util.List;
-
-public interface IAStarPathedEntity {
-    void onFoundPath(List<AStarNode> var1);
-
-    void onNoPathAvailable();
-}
